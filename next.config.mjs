488346cdf7e590a72f4d6a7399d77c6ae37cbdc8
22a37.deeksha@sjec.ac.in=Rakesh <rakeshpoojary850@gmail.com>
2025-07-08 19:26:10 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // important for static hosting
+  }
+};
 
-export default nextConfig;
+module.exports = nextConfig;
